52afa39245730855ad88c2e46f2be25890e371fb
starones <itstarone172@gmail.com>
2022-01-10 07:41:10 +0000

    [Add]job_request/new

--- app/controllers/client/job_requests_controller.rb
+++ app/controllers/client/job_requests_controller.rb
@@ -1,5 +1,7 @@
 class Client::JobRequestsController < ApplicationController
+
   def index
+
   end
 
   def show
@@ -9,14 +11,24 @@ class Client::JobRequestsController < ApplicationController
   end
 
   def new
+    @job_request = JobRequest.new
   end
 
   def create
+    @job_request = JobRequest.new(job_request_params)
+    @job_request.client_id = current_client.id
+    @job_request.save
+    redirect_to client_job_requests_path
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+  def job_request_params
+    params.require(:job_request).permit(:title, :job_content, :recruitment_gender, :recruitment_stature, :recruitmebt_other, :money, :contact)
   end
 end
