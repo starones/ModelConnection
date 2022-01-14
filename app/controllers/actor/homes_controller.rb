2fa37504b199b59c0b994d2a9e3ce5459d588078
starones <itstarone172@gmail.com>
2022-01-14 03:24:58 +0000

    [Add]rank(top/viwe)

--- app/controllers/actor/homes_controller.rb
+++ app/controllers/actor/homes_controller.rb
@@ -1,6 +1,7 @@
 class Actor::HomesController < ApplicationController
 
   def top
+    @ranks = Actor.find(Favorite.group(:actor_id).order('count(actor_id) desc').limit(4).pluck(:actor_id))
     @actors = Actor.order(created_at: :desc).limit(4)
   end
 
