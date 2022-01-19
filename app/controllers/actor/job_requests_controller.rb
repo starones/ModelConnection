3f33d081e9606f84e53c7fe85f654b97839621d6
starones <itstarone172@gmail.com>
2022-01-19 02:47:06 +0000

    [Fix]actor/job_request/viwes

--- app/controllers/actor/job_requests_controller.rb
+++ app/controllers/actor/job_requests_controller.rb
@@ -1,7 +1,7 @@
 class Actor::JobRequestsController < ApplicationController
 
   def index
-    @job_requests = JobRequest.all.order(created_at: :desc)
+    @job_requests = JobRequest.all.order(created_at: :desc).page(params[:page]).per(14)
   end
 
   def show
