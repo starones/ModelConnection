7922cf4afb97b6e25754812e314e04dded5febc9
starones <itstarone172@gmail.com>
2022-01-20 15:20:52 +0000

    [Fix]viwes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,26 @@ class ApplicationController < ActionController::Base
    # ____devise名前新規登録追加____
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    case resource
+    when Client
+      client_client_path(@client)
+    when Actor
+      actor_path(@actor)
+    end
+  end
+
+  def after_sign_up_path_for(resource)
+    case resource
+    when Client
+      client_client_path(@client)
+    when Actor
+      actor_path(@actor)
+    end
+  end
+
+
+
   protected
 
   def configure_permitted_parameters
