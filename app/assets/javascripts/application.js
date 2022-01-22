6f839712f3b939ece85e4b57c6429b7d6ebb0c1b
starones <itstarone172@gmail.com>
2022-01-22 15:33:42 +0000

    [Add]javascriptのテキスト

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -32,10 +32,48 @@
       childrenElementType : 'div',
       arrows : false,
       autoPlay : true,
-      autoPlayDuration : 3500,
+      autoPlayDuration : 6000,
       keyboardOnAlways : true,
       hidePrevious : false
     });
   });
 });
 // ------ここまで--------
+
+// top画面文字デザイン（理解は全くしていない）
+function EachTextAnimeControl() {
+  $('.eachTextAnime').each(function () {
+    var elemPos = $(this).offset().top - 50;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass("appeartext");
+
+    } else {
+      $(this).removeClass("appeartext");
+    }
+  });
+}
+
+$(window).on('load', function () {
+  //spanタグを追加する
+  var element = $(".eachTextAnime");
+  element.each(function () {
+    var text = $(this).text();
+    var textbox = "";
+    text.split('').forEach(function (t, i) {
+      if (t !== " ") {
+        if (i < 10) {
+          textbox += '<span style="animation-delay:.' + i + 's;">' + t + '</span>';
+        } else {
+          var n = i / 10;
+          textbox += '<span style="animation-delay:' + n + 's;">' + t + '</span>';
+        }
+      } else {
+        textbox += t;
+      }
+    });
+    $(this).html(textbox);
+  });
+  EachTextAnimeControl();
+});
