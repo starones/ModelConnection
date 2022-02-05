941228c81e8c748ebd9b331de1e172bd1f63aebc
starones <itstarone172@gmail.com>
2022-02-05 12:41:24 +0000

    [Add]admin/views

--- app/controllers/admin/actors_controller.rb
+++ app/controllers/admin/actors_controller.rb
@@ -1,2 +1,16 @@
 class Admin::ActorsController < ApplicationController
+    before_action :authenticate_admin!, except: [:index, :show]
+    
+    def index
+    end
+    
+    def show
+    end
+    
+    def edit
+    end
+    
+    def update
+    end
+    
 end
--- app/controllers/admin/clients_controller.rb
+++ app/controllers/admin/clients_controller.rb
@@ -1,2 +1,16 @@
 class Admin::ClientsController < ApplicationController
+    before_action :authenticate_admin!, except: [:index, :show]
+    
+    def index
+    end
+    
+    def show
+    end
+    
+    def edit
+    end
+    
+    def update
+    end
+    
 end
