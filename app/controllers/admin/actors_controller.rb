6ee784e3647d846b7dedd6ec89fd84e753adf431
starones <itstarone172@gmail.com>
2022-02-05 14:12:43 +0000

    [Add]admin/actor

--- app/controllers/admin/actors_controller.rb
+++ app/controllers/admin/actors_controller.rb
@@ -1,16 +1,32 @@
 class Admin::ActorsController < ApplicationController
-    before_action :authenticate_admin!, except: [:index, :show]
-    
+    before_action :authenticate_admin!, except: [:index, :show, :edit]
+
     def index
+      @actors = Actor.all.page(params[:page]).per(20)
     end
-    
+
     def show
+      @actor = Actor.find(params[:id])
     end
-    
+
     def edit
+      @actor = Actor.find(params[:id])
     end
-    
+
     def update
+      @actor = Actor.find(params[:id])
+      if @actor.update(actor_params)
+        redirect_to admin_actor_path(@actor.id)
+      else
+        render "edit"
+      end
     end
-    
+
+    private
+
+    def actor_params
+      params.require(:actor).permit(:name, :email, :image, :born_year, :born_month, :born_day, :age, :body, :career,
+      :contact, :stature, :gender, :instagram_rink, :twitter_rink, :tiktok_rink, :others_rink)
+    end
+
 end
